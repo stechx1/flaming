19911970a12464549d95eb7c63b51746a5607b46
saqib867 <saqibhussain9910@gmail.com>
2024-01-23 01:08:26 +0500

    some changes pushed to front-end

--- components/Products/ProductItem.js
+++ components/Products/ProductItem.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Radio, Select } from "antd";
+import { Button, Radio, Select } from "antd";
 import CustomizationForm from "./CustomizationForm";
 import { baseImgUri } from "@/constants/baseImgUri";
 import { axiosInstance } from "@/axios/axios";
@@ -24,15 +24,10 @@ const ProductItem = () => {
     title: "",
   });
 
-  const totalPrice =
-    +price +
-    selectedWeatherproofing +
-    selectedHanging +
-    selectedEdging +
-    selectedImageOption +
-    selectedPostage;
+  const totalPrice = price 
 
   useEffect(() => {
+    if (productId) {
     const getProductDetails = async () => {
       try {
         const response = await axiosInstance.get(
@@ -44,16 +39,20 @@ const ProductItem = () => {
         setItemDetails({
           title: getData?.title,
           heroImg: getData?.heroImg?.data?.attributes?.url,
-          childImages: getData?.childImages?.data,
+          childImages: getData?.gallery?.data,
           description: getData.description,
-          base_price_description: getData?.base_price_description,
+          size:getData?.initial_size,
+          is_tember_sign:getData?.categories_id?.data?.attributes?.is_tember_sign
+         
         });
       } catch (error) {
         console.log("failed to fetch data", error);
       }
     };
-
+    
     getProductDetails();
+  }
+
   }, [productId]);
 
   return (
@@ -71,37 +70,43 @@ const ProductItem = () => {
           <span className="text-[#003933] text-[24px] sm:text-[40px] font-[600]">
             {ItemDetails.title}
           </span>
-          <span className="text-[#003933] font-bold text-lg sm:text-4xl">
-            $ {parseInt(totalPrice)}
+          <span className="text-[#003933] font-bold text-lg sm:text-2xl">
+           Price - $ {parseInt(totalPrice)}
           </span>
-          <span>
-            Base Price - $ {price} {ItemDetails?.base_price_description}
+          
+          <span className="text-[#003933] font-bold text-lg sm:text-[20px]">
+            Size - {ItemDetails?.size}
           </span>
 
           <p className="text-gray-600">{ItemDetails?.description}</p>
-
-          <p className="text-gray-600">
-            Below are some examples of the wood timber signs, go through them
-            so, you better know, what option to select.
-          </p>
+          
         </div>
       </div>
-
-      <div className="my-12">
-        <h2 className="text-3xl font-bold mb-5">Pictures</h2>
-        <div className="grid grid-cols-2 sm:grid-cols-4 gap-8">
+      <div className="flex flex-col sm:flex-wrap md:flex-row items-center justify-center gap-x-3 gap-y-3 mt-[50px] ">
+          <button className="bg-[#003933] text-white py-2 px-2 rounded w-full md:w-[270px] text-xl md:text-xl" >Custome Signs</button>
+          <button className="bg-[#003933] text-white py-2 px-2 rounded w-full md:w-[270px] text-xl md:text-xl" >Design Your Own Sign</button>
+          {ItemDetails?.is_tember_sign && <button className="bg-[#003933] text-white py-2 px-2 rounded w-full md:w-[270px] text-xl md:text-xl" >Sign Pricing Guide</button>}
+          <div></div>
+          <div></div>
+      </div>
+      <div className="my-12 flex flex-col gap-y-2">
+        <h2 className="text-3xl font-bold mb-5">Gallery</h2>
+        <p className="text-gray-600 text-lg">
+            Below are some picture of the sign
+          </p>
+        <div className="flex flex-wrap gap-8">
           {ItemDetails.childImages?.map((picture, index) => {
             return (
               <img
                 key={index}
                 src={`${baseImgUri}${picture?.attributes?.url}`}
-                className="rounded"
+                className="rounded max-w-[420px] flex-1 bg-cover"
               />
             );
           })}
         </div>
       </div>
-      <span className="text-[#003933] font-bold text-base sm:text-4xl">
+      <span className="text-[#003933] font-bold text-base sm:text-4xl hidden">
         Price: ${" "}
         {+price +
           +selectedWeatherproofing +
@@ -110,7 +115,7 @@ const ProductItem = () => {
           +selectedImageOption +
           +selectedPostage}
       </span>
-      <div className="p-5 my-12 flex flex-col gap-6 border xl:w-1/2 border-[#BDBDBD] rounded-xl">
+      <div className="p-5 my-12 hidden flex-col gap-6 border xl:w-1/2 border-[#BDBDBD] rounded-xl " >
         <div className="flex gap-6 items-center">
           <p className="text-[#003933] font-bold my-3">Choose Size</p>
           <Select
